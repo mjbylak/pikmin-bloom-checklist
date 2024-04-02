df1224eb0a827977ef0e6fed75c461e114f07f54
Mitchell Bylak <78622329+mjbylak@users.noreply.github.com>
2024-04-02 14:37:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 const withPWA = require("next-pwa")({
   dest: "public",
-  basePath: "/pikmin-bloom-checklist",
 });
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   reactStrictMode: true,
   images: {
     loader: "custom",
-    path: "/",
+    path: isProd ? "/pikmin-bloom-checklist/" : "/",
   },
+  // Set up basePath and assetPrefix for GitHub Pages
+  basePath: isProd ? '/pikmin-bloom-checklist' : '',
+  assetPrefix: isProd ? '/pikmin-bloom-checklist/' : '',
 });
