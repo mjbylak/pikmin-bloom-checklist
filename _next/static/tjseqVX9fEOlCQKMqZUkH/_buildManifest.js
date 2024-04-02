8ea8bebb309c525bf0365a308936996110a4d555
mjbylak <78622329+mjbylak@users.noreply.github.com>
2024-04-02 12:38:13 +0000

    Deploying to gh-pages from @ mjbylak/pikmin-bloom-checklist@df1224eb0a827977ef0e6fed75c461e114f07f54 🚀

--- _next/static/tjseqVX9fEOlCQKMqZUkH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/329-3e0015cbca7ef307.js","static/css/1cd96843d76805b7.css","static/chunks/pages/index-75696362e08de2f7.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
